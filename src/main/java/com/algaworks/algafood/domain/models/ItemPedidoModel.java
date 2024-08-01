4e68d42d25135383f3ebd23cd550a54866e8f3e7
igor-lourenco <igor.lourencosantos@gmail.com>
2024-08-01 09:19:55 -0300

    Criação do model ItemPedidoModel

---
+++ src/main/java/com/algaworks/algafood/domain/models/ItemPedidoModel.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.domain.models;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedidoModel {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private PedidoModel pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private ProdutoModel produto;
+}
