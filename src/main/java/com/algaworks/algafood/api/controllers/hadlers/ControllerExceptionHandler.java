495de15776f7956de3fc0fafc5fd556b8137081c
igor-lourenco <igor.lourencosantos@gmail.com>
2024-08-05 14:45:39 -0300

    Customizando o corpo de resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/controllers/hadlers/ControllerExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/controllers/hadlers/ControllerExceptionHandler.java
@@ -3,53 +3,59 @@ package com.algaworks.algafood.api.controllers.hadlers;
 import com.algaworks.algafood.api.controllers.exceptions.StandardError;
 import com.algaworks.algafood.domain.exceptions.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exceptions.EntidadeNaoEncontradaException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+
+    @Override // sobrescreve o método para retornar nosso body de resposta padrão
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if(body == null) {
+            body = StandardError.builder().dataHora(LocalDateTime.now()).mensagem(ex.getMessage()).build();
+        }else if(body instanceof String){
+            body = StandardError.builder().dataHora(LocalDateTime.now()).mensagem((String)body).build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<StandardError> entidadeEmUsoExceptionHandler(EntidadeEmUsoException e, HttpServletRequest request){
+    public ResponseEntity<?> entidadeEmUsoExceptionHandler(EntidadeEmUsoException e, WebRequest request){
 
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        StandardError err = StandardError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 
-        return ResponseEntity.status(status).body(err);
+//        HttpStatus status = HttpStatus.BAD_REQUEST;
+//        StandardError err = StandardError.builder()
+//                .dataHora(LocalDateTime.now())
+//                .mensagem(e.getMessage())
+//                .build();
+
+//        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<StandardError> entidadeNaoEncontradaExceptionHandler(EntidadeNaoEncontradaException e, HttpServletRequest request){
+    public ResponseEntity<?> entidadeNaoEncontradaExceptionHandler(EntidadeNaoEncontradaException e, WebRequest request){
 
-        HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = StandardError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,  request);
 
-        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<StandardError> exceptionHandler(Exception e, HttpServletRequest request){
+    public ResponseEntity<?> exceptionHandler(Exception e, WebRequest request){
 
         e.printStackTrace();
 
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        StandardError err = StandardError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(status).body(err);
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,  request);
     }
 }
