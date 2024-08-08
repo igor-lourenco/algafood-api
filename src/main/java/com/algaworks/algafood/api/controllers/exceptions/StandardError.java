9f42ed20d4f85a489ff207ce428a96c91606d0d2
igor-lourenco <igor.lourencosantos@gmail.com>
2024-08-08 15:05:18 -0300

    Conhecendo o RFC 7807(Problem Details) para APIs

--- src/main/java/com/algaworks/algafood/api/controllers/exceptions/StandardError.java
+++ src/main/java/com/algaworks/algafood/api/controllers/exceptions/StandardError.java
@@ -1,14 +1,19 @@
 package com.algaworks.algafood.api.controllers.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL) // remove os campos nulos no retorno da resposta
 @Getter
 @Builder
-public class StandardError {
+public class StandardError { // corpo de resposta padrão RFC 7807
 
-    private LocalDateTime dataHora;
-    private String mensagem;
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/algaworks/algafood/api/controllers/hadlers/ControllerExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/controllers/hadlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controllers.hadlers;
 
 import com.algaworks.algafood.api.controllers.exceptions.StandardError;
+import com.algaworks.algafood.api.controllers.exceptions.enums.ErrorType;
 import com.algaworks.algafood.domain.exceptions.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exceptions.EntidadeNaoEncontradaException;
 import org.springframework.http.HttpHeaders;
@@ -8,10 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
@@ -22,40 +23,46 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         if(body == null) {
-            body = StandardError.builder().dataHora(LocalDateTime.now()).mensagem(ex.getMessage()).build();
+            body = StandardError.builder().timestamp(LocalDateTime.now()).status(status.value()).title(ex.getMessage()).build();
         }else if(body instanceof String){
-            body = StandardError.builder().dataHora(LocalDateTime.now()).mensagem((String)body).build();
+            body = StandardError.builder().timestamp(LocalDateTime.now()).status(status.value()).title((String)body).build();
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> entidadeEmUsoExceptionHandler(EntidadeEmUsoException e, WebRequest request){
+    public ResponseEntity<?> handlerEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request){
 
         return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 
-//        HttpStatus status = HttpStatus.BAD_REQUEST;
-//        StandardError err = StandardError.builder()
-//                .dataHora(LocalDateTime.now())
-//                .mensagem(e.getMessage())
-//                .build();
-
-//        return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> entidadeNaoEncontradaExceptionHandler(EntidadeNaoEncontradaException e, WebRequest request){
+    public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorType errorType = ErrorType.ENTIDADE_NAO_ENCONTRADA;
 
-        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,  request);
+        StandardError error = createStandardErrorBuilder(status, errorType, e.getMessage()).build();
+
+        return handleExceptionInternal(e, error, new HttpHeaders(), status,  request);
 
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> exceptionHandler(Exception e, WebRequest request){
+    public ResponseEntity<?> handleErrorException(Exception e, WebRequest request){
 
         e.printStackTrace();
 
         return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,  request);
+    }
+
+    private StandardError.StandardErrorBuilder createStandardErrorBuilder(HttpStatus status, ErrorType errorType, String detail) {
+
+        return StandardError.builder()
+                .timestamp(LocalDateTime.now())
+                .type(errorType.getUri())
+                .title(errorType.getTitle())
+                .detail(detail);
     }
 }
