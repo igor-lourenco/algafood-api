481b95e04694efe22de26bfcfa637d0993f05c74
igor-lourenco <igor.lourencosantos@gmail.com>
2024-08-08 15:10:43 -0300

    Padronizando o formato de problemas no corpo de resposta com RFC 7807

--- src/main/java/com/algaworks/algafood/api/controllers/exceptions/enums/ErrorTypeEnum.java
+++ src/main/java/com/algaworks/algafood/api/controllers/exceptions/enums/ErrorTypeEnum.java
@@ -1,17 +1,16 @@
 package com.algaworks.algafood.api.controllers.exceptions.enums;
 
 import lombok.Getter;
-import org.apache.tomcat.jni.Error;
 
 @Getter
-public enum ErrorType {
+public enum ErrorTypeEnum {
 
     ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada");
 
     private String title;
     private String uri;
 
-    ErrorType(String title, String path){
+    ErrorTypeEnum(String title, String path){
         this.title = title;
         this.uri = "http://localhost:8080" + path;
     }
--- src/main/java/com/algaworks/algafood/api/controllers/hadlers/ControllerExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/controllers/hadlers/ControllerExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.api.controllers.hadlers;
 
 import com.algaworks.algafood.api.controllers.exceptions.StandardError;
-import com.algaworks.algafood.api.controllers.exceptions.enums.ErrorType;
+import com.algaworks.algafood.api.controllers.exceptions.enums.ErrorTypeEnum;
 import com.algaworks.algafood.domain.exceptions.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exceptions.EntidadeNaoEncontradaException;
 import org.springframework.http.HttpHeaders;
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -22,10 +21,10 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @Override // sobrescreve o método para retornar nosso body de resposta padrão
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-        if(body == null) {
+        if (body == null) {
             body = StandardError.builder().timestamp(LocalDateTime.now()).status(status.value()).title(ex.getMessage()).build();
-        }else if(body instanceof String){
-            body = StandardError.builder().timestamp(LocalDateTime.now()).status(status.value()).title((String)body).build();
+        } else if (body instanceof String) {
+            body = StandardError.builder().timestamp(LocalDateTime.now()).status(status.value()).title((String) body).build();
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
@@ -41,7 +40,7 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest request){
         HttpStatus status = HttpStatus.NOT_FOUND;
-        ErrorType errorType = ErrorType.ENTIDADE_NAO_ENCONTRADA;
+        ErrorTypeEnum errorType = ErrorTypeEnum.ENTIDADE_NAO_ENCONTRADA;
 
         StandardError error = createStandardErrorBuilder(status, errorType, e.getMessage()).build();
 
@@ -57,7 +56,7 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,  request);
     }
 
-    private StandardError.StandardErrorBuilder createStandardErrorBuilder(HttpStatus status, ErrorType errorType, String detail) {
+    private StandardError.StandardErrorBuilder createStandardErrorBuilder(HttpStatus status, ErrorTypeEnum errorType, String detail) {
 
         return StandardError.builder()
                 .timestamp(LocalDateTime.now())
