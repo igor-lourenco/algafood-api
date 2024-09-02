eb7158e64111cc7d7974d07cc5fa155f15c492ad
igor-lourenco <igor.lourencosantos@gmail.com>
2024-09-02 10:05:27 -0300

    Criacao da classe - RestauranteProdutoController

---
+++ src/main/java/com/algaworks/algafood/api/controllers/RestauranteProdutoController.java
@@ -0,0 +1,56 @@
+package com.algaworks.algafood.api.controllers;
+
+import com.algaworks.algafood.api.DTOs.FormaPagamentoDTO;
+import com.algaworks.algafood.api.DTOs.ProdutoDTO;
+import com.algaworks.algafood.api.inputs.ProdutoInput;
+import com.algaworks.algafood.domain.services.RestauranteFormaPagamentoService;
+import com.algaworks.algafood.domain.services.RestauranteProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/restaurantes/{restauranteId}/produtos", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+public class RestauranteProdutoController {
+
+    @Autowired
+    private RestauranteProdutoService service;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> findAll(@PathVariable(value = "restauranteId") Long restauranteId){
+        List<ProdutoDTO> produtoDTOS = service.findAllProdutos(restauranteId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(produtoDTOS);
+
+    }
+
+
+    @GetMapping(value = "/{produtoId}")
+    public ResponseEntity<ProdutoDTO> findByProdutoId(
+        @PathVariable(value = "restauranteId") Long restauranteId,
+        @PathVariable(value = "produtoId") Long produtoId){
+
+        ProdutoDTO produtoDTO = service.findAllProdutosById(restauranteId, produtoId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(produtoDTO);
+
+    }
+
+
+    @PutMapping(value = "/{produtoId}")
+    public ResponseEntity<ProdutoDTO> saveProduto(
+        @PathVariable(value = "restauranteId") Long restauranteId,
+        @PathVariable(value = "produtoId") Long produtoId,
+        @Valid @RequestBody ProdutoInput produtoInput){
+
+        ProdutoDTO produtoDTO = service.alteraProdutoByRestauranteId(restauranteId, produtoId, produtoInput);
+
+        return ResponseEntity.status(HttpStatus.OK).body(produtoDTO);
+
+    }
+}
