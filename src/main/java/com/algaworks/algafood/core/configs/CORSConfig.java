ca68c0b39a7d8989574a551dc6e54fa4413daabc
igor-lourenco <igor.lourencosantos@gmail.com>
2024-10-08 11:07:59 -0300

    Habilitando CORS globalmente na aplicação

---
+++ src/main/java/com/algaworks/algafood/core/configs/CORSConfig.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.core.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/** Essa classe configura as políticas de CORS (Cross-Origin Resource Sharing) para a aplicação */
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry
+            .addMapping("/**") // Está aplicando as configurações de CORS dessa classe a todas as rotas/endpoints da aplicação
+            .allowedOrigins("*") // Está permitindo solicitações de qualquer origem
+            .allowedMethods("*") // Está permitindo todos os métodos HTTP (GET, POST, PUT, DELETE, etc.) nas requisições CORS.
+            .maxAge(30); //  Tempo (em segundos) que o navegador deve armazenar em cache a resposta da requisição de preflight (verificação prévia que o navegador faz para saber se a requisição real é segura).
+    }
+}
