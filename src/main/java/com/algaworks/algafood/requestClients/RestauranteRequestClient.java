06ba00595096d1679c533bc00f10a6569c08ce76
igor-lourenco <igor.lourencosantos@gmail.com>
2024-10-09 09:51:30 -0300

    Tratando respostas com código de erro

--- src/main/java/com/algaworks/algafood/requestClients/RestauranteRequestClient.java
+++ src/main/java/com/algaworks/algafood/requestClients/RestauranteRequestClient.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.requestClients;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 public class RestauranteRequestClient {
@@ -16,24 +19,44 @@ public class RestauranteRequestClient {
 
     public static void main(String[] args) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        String baseUrl = "http://localhost:8080";
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String baseUrl = "http://localhost:8080/";
 
-        RestauranteRequestClient requestClient = new RestauranteRequestClient(baseUrl, restTemplate);
+            RestauranteRequestClient requestClient = new RestauranteRequestClient(baseUrl, restTemplate);
 
-        var response = requestClient.listar();
-        response.forEach(System.out::println);
+            var response = requestClient.listar();
+            response.forEach(System.out::println);
 
+        } catch (RestauranteRequestClientException e) {
+
+            if (e.getStandardError() != null) {
+                System.out.println(e.getStandardError().getDetail());
+            } else {
+                System.out.println("Erro desconhecido: " + e.getMessage());
+            }
+        }
 
     }
 
 
-    public List<RestauranteResumoRequestClientDTO> listar(){
-        URI resourceUri = URI.create(url + RESOURCE_PATH);
+    public List<RestauranteResumoRequestClientDTO> listar() {
 
-        RestauranteResumoRequestClientDTO[] restaurantes = restTemplate
-            .getForObject(resourceUri, RestauranteResumoRequestClientDTO[].class);
+        try {
+            URI resourceUri = URI.create(url + RESOURCE_PATH);
 
-        return Arrays.asList(restaurantes);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<RestauranteResumoRequestClientDTO[]> restaurantes = restTemplate.exchange(
+                resourceUri, HttpMethod.GET, entity, RestauranteResumoRequestClientDTO[].class);
+
+//          Se o 'restaurantes.getBody()' for null retorna a exception NullPointerException com a mensagem personalizada 'A resposta da requisição não pode ser null'
+            return Arrays.asList(Objects.requireNonNull(restaurantes.getBody(), "A resposta da requisição não pode ser null"));
+        } catch (RestClientResponseException e) {
+            throw new RestauranteRequestClientException(e.getMessage(), e);
+        }
     }
 }
---
+++ src/main/java/com/algaworks/algafood/requestClients/RestauranteRequestClientException.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.requestClients;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.Data;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestClientResponseException;
+
+@Getter
+@Slf4j
+public class RestauranteRequestClientException extends RuntimeException {
+
+    private StandardError standardError;
+
+    public RestauranteRequestClientException(String message, RestClientResponseException cause) {
+        super(message, cause);
+
+        deserializeStandardError(cause);
+    }
+
+
+    public void deserializeStandardError(RestClientResponseException e){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+
+        objectMapper.findAndRegisterModules();
+
+        try {
+            this.standardError = objectMapper.readValue(e.getResponseBodyAsString(), StandardError.class);
+        } catch (JsonProcessingException ex) {
+            log.warn("Não foi possível desserializar a resposta em uma StandardError.class :: " + ex.getMessage());
+        }
+    }
+}
+
+
+@Data
+class StandardError {
+    private Integer status;
+    private String timestamp;
+    private String detail;
+}
