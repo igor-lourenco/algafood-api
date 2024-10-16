26c2acc3ef4cd2b1672d05020a58d7cbc42fc4ce
igor-lourenco <igor.lourencosantos@gmail.com>
2024-10-16 10:40:42 -0300

    Gerando a documentação da API em HTML com Swagger UI e SpringFox

--- src/main/java/com/algaworks/algafood/core/configs/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/configs/SpringFoxConfig.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.core.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -11,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * nos controladores e endpoints presentes na aplicação */
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket apiDocket() {
@@ -20,5 +22,16 @@ public class SpringFoxConfig {
             .select() // Inicia um builder que permite especificar quais controladores e endpoints serão incluídos na documentação.
             .apis(RequestHandlerSelectors.any()) // Configura o Docket para incluir todos os controladores disponíveis na aplicação.
             .build(); // Finaliza a configuração do Docket e retorna a instância pronta para ser gerenciada pelo Spring.
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//      arquivo jar: springfox-swagger-ui
+        registry.addResourceHandler("swagger-ui.html") // url da documentação: http://localhost:8080/swagger-ui.html
+            .addResourceLocations("classpath:/META-INF/resources/"); // caminho onde está o arquivo swagger-ui.html
+
+        registry.addResourceHandler("/webjars/**") // mapeia os arquivos estáticos css, javascript, ícones e afins do Swagger
+            .addResourceLocations("classpath:/META-INF/resources/webjars/"); // caminho onde está os arquivos estáticos
+
     }
 }
