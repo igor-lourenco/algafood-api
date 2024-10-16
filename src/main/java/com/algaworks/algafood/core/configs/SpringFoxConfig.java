f9b5da149463bfcc5436b2f3d781b1b973b8a3af
igor-lourenco <igor.lourencosantos@gmail.com>
2024-10-16 10:56:52 -0300

    Selecionando os endpoints da API para gerar a documentação

--- src/main/java/com/algaworks/algafood/core/configs/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/configs/SpringFoxConfig.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.core.configs;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,7 +22,16 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
         return new Docket(DocumentationType.SWAGGER_2) // Cria uma instância de Docket configurada para utilizar o tipo de documentação Swagger 2.0
             .select() // Inicia um builder que permite especificar quais controladores e endpoints serão incluídos na documentação.
-            .apis(RequestHandlerSelectors.any()) // Configura o Docket para incluir todos os controladores disponíveis na aplicação.
+
+//          .apis(RequestHandlerSelectors.any()) // Configura o Docket para incluir todos os controladores disponíveis na aplicação.
+            .apis(Predicates.and(
+                RequestHandlerSelectors.basePackage("com.algaworks.algafood.api.controllers") // especifica o pacote onde estão os controladores da aplicação.
+//                ,RequestHandlerSelectors.basePackage("com.algaworks.algafood.api.outroController") // se quiser adicionar outro pacote de controller
+            ))
+
+            .paths(PathSelectors.any()) // Especifica que qualquer endpoint da Aplicação será documentado.
+//          .paths(PathSelectors.ant("/restaurantes/*")) // Especifica que somente os endpoints que começam com /restaurantes/ e têm exatamente um segmento adicional serão documentados.
+
             .build(); // Finaliza a configuração do Docket e retorna a instância pronta para ser gerenciada pelo Spring.
     }
 
