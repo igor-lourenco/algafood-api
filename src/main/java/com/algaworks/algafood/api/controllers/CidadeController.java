309fcdd303d4bf7d70af8685e159b5ab641acdc4
igor-lourenco <igor.lourencosantos@gmail.com>
2024-10-18 14:56:21 -0300

    Descrevendo códigos de status de respostas ena API especifica de CidadeController

--- src/main/java/com/algaworks/algafood/api/controllers/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/CidadeController.java
@@ -2,11 +2,10 @@ package com.algaworks.algafood.api.controllers;
 
 
 import com.algaworks.algafood.api.DTOs.CidadeDTO;
+import com.algaworks.algafood.api.controllers.exceptions.StandardError;
 import com.algaworks.algafood.api.inputs.CidadeInput;
 import com.algaworks.algafood.domain.services.CidadeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,8 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@Api(tags = "Cidades")
-// Mapeia essa tag "Cidades" com a tag declarada na classe SpringFoxConfig da aplicação, para ser visualizada na documentação.
+@Api(tags = "Cidades") // Mapeia essa tag "Cidades" com a tag declarada na classe SpringFoxConfig da aplicação, para ser visualizada na documentação.
 @RestController
 @RequestMapping(value = "/cidades", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class CidadeController {
@@ -25,12 +23,19 @@ public class CidadeController {
     @Autowired
     private CidadeService cidadeService;
 
-    @ApiOperation("Busca lista de todas as cidades")
+
+    @ApiOperation(value = "Busca lista de todas as cidades")
+    @ApiModelProperty(position = 0)
     @GetMapping
     public ResponseEntity<List<CidadeDTO>> listar() {
         return ResponseEntity.status(HttpStatus.OK).body(cidadeService.listar());
     }
 
+
+    @ApiResponses({
+        @ApiResponse(code = 400, message = "ID da cidade inválido", response = StandardError.class),
+        @ApiResponse(code = 404, message = "Cidade não encontrada", response = StandardError.class)
+    })
     @ApiOperation(value = "Busca cidade pelo ID")
     @GetMapping(value = "/{id}")
     public ResponseEntity<CidadeDTO> buscaPorId(
@@ -39,6 +44,7 @@ public class CidadeController {
         CidadeDTO cidadeDTO = cidadeService.buscaPorId(id);
         return ResponseEntity.status(HttpStatus.OK).body(cidadeDTO);
     }
+
 
     @ApiOperation("Busca lista de cidades pelo nome")
     @GetMapping(value = "/consulta-por-nome")
@@ -49,6 +55,9 @@ public class CidadeController {
         return ResponseEntity.status(HttpStatus.OK).body(listaCidadePorNome);
     }
 
+    @ApiResponses({
+        @ApiResponse(code = 201, message = "Cidade cadastrada")
+    })
     @ApiOperation("Cadastra uma nova cidade")
     @PostMapping
     public ResponseEntity<CidadeDTO> salvar(
@@ -58,6 +67,10 @@ public class CidadeController {
         return ResponseEntity.status(HttpStatus.CREATED).body(cidadeDTO);
     }
 
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Cidade atualizada"),
+        @ApiResponse(code = 404, message = "Cidade não encontrada", response = StandardError.class)
+    })
     @ApiOperation("Atualiza cidade pelo ID")
     @PutMapping(value = "/{id}")
     public ResponseEntity<CidadeDTO> alterar(
@@ -68,14 +81,18 @@ public class CidadeController {
         return ResponseEntity.status(HttpStatus.OK).body(cidadeDTO);
     }
 
+
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Cidade deletada"),
+        @ApiResponse(code = 404, message = "Cidade não encontrada", response = StandardError.class)
+    })
     @ApiOperation(value = "Exclui cidade pelo ID")
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
-    public ResponseEntity<?> deletar(
+    public void deletar(
         @ApiParam(name = "id", value = "ID da cidade", example = "1") @PathVariable(value = "id") Long id) {
 
         cidadeService.deletar(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
