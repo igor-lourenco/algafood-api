87200bdff23ea09a32a70302f69af2abb3908b57
igor-lourenco <igor.lourencosantos@gmail.com>
2024-11-05 10:39:11 -0300

    Descrevendo a documentacao das APIs - Permissao do Grupo(GrupoPermissao)

--- src/main/java/com/algaworks/algafood/api/DTOs/GrupoPermissaoDTO.java
+++ src/main/java/com/algaworks/algafood/api/DTOs/GrupoPermissaoDTO.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.api.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "Permissão do Grupo") // Usada no contexto do Swagger para descrever essa classe como modelo de dados que será utilizado na API
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,7 +16,13 @@ import lombok.NoArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL) // Ignora campos com valores nulos durante a serialização para JSON
 public class GrupoPermissaoDTO {
 
+//  @ApiModelProperty(value = "ID do grupo", example = "1")
+    @ApiModelProperty(example = "1", position = 0)
     private Long id;
+
+    @ApiModelProperty(example = "CONSULTAR_COZINHAS", position = 5)
     private String nome;
+
+    @ApiModelProperty(example = "Permite consultar cozinhas", position = 10)
     private String descricao;
 }
--- src/main/java/com/algaworks/algafood/api/controllers/GrupoPermissaoController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/GrupoPermissaoController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controllers;
 
 import com.algaworks.algafood.api.DTOs.GrupoPermissaoDTO;
 import com.algaworks.algafood.domain.services.GrupoPermissaoService;
+import com.algaworks.algafood.swaggerOpenApi.controllers.GrupoPermissaoControllerOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,49 +12,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/grupos/{grupoId}/permissoes", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-public class GrupoPermissaoController {
+@RequestMapping(value = "/grupos/{grupoId}/permissoes", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
 
     @Autowired
     private GrupoPermissaoService grupoPermissaoService;
 
     @GetMapping
-    public ResponseEntity<List<GrupoPermissaoDTO>> findAllPermissoes(@PathVariable(value = "grupoId") Long grupoId) {
+    public ResponseEntity<List<GrupoPermissaoDTO>> buscaTodasPermissoesDoGrupo(@PathVariable(value = "grupoId") Long grupoId) {
+
         List<GrupoPermissaoDTO> grupoDTOS = grupoPermissaoService.findAllPermissoes(grupoId);
         return ResponseEntity.status(HttpStatus.OK).body(grupoDTOS);
-
     }
 
 
     @GetMapping(value = "/{permissaoId}")
-    public ResponseEntity<GrupoPermissaoDTO> findPermissaoByPermissaoId(
+    public ResponseEntity<GrupoPermissaoDTO> buscaPermissaoDoGrupoPeloId(
         @PathVariable(value = "grupoId") Long grupoId,
         @PathVariable(value = "permissaoId") Long permissaoId) {
 
         GrupoPermissaoDTO grupoDTOS = grupoPermissaoService.findPermissaoByPermissaoId(grupoId, permissaoId);
         return ResponseEntity.status(HttpStatus.OK).body(grupoDTOS);
-
     }
 
 
     @PutMapping(value = "/{permissaoId}")
-    public ResponseEntity<?> associaPermissao(
+    public ResponseEntity<Void> associaPermissao(
         @PathVariable(value = "grupoId") Long grupoId,
         @PathVariable(value = "permissaoId") Long permissaoId) {
 
         grupoPermissaoService.associaPermissao(grupoId, permissaoId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
     }
 
 
     @DeleteMapping(value = "/{permissaoId}")
-    public ResponseEntity<?> desassociaPermissao(
+    public ResponseEntity<Void> desassociaPermissao(
         @PathVariable(value = "grupoId") Long grupoId,
         @PathVariable(value = "permissaoId") Long permissaoId) {
 
         grupoPermissaoService.desassociaPermissao(grupoId, permissaoId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
     }
 }
