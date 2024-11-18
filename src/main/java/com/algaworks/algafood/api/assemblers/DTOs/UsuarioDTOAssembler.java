a1a488d62e8a2a6cff0c685cbfbd36b850b821bb
igor-lourenco <igor.lourencosantos@gmail.com>
2024-11-18 10:09:08 -0300

    Externalizando implementacao dos links da API - Usuario

--- src/main/java/com/algaworks/algafood/api/assemblers/DTOs/UsuarioDTOAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assemblers/DTOs/UsuarioDTOAssembler.java
@@ -1,15 +1,13 @@
 package com.algaworks.algafood.api.assemblers.DTOs;
 
 import com.algaworks.algafood.api.DTOs.UsuarioDTO;
+import com.algaworks.algafood.api.assemblers.links.UsuarioLinks;
 import com.algaworks.algafood.api.controllers.UsuarioController;
-import com.algaworks.algafood.api.controllers.UsuarioGrupoController;
 import com.algaworks.algafood.domain.models.UsuarioModel;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,43 +15,34 @@ public class UsuarioDTOAssembler extends RepresentationModelAssemblerSupport<Usu
 
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private UsuarioLinks usuarioLinks;
+
 
     //  Construtor obrigatório para criar um novo RepresentationModelAssemblerSupport usando a classe de controlador e o tipo de recurso fornecidos como base.
     public UsuarioDTOAssembler() {
         super(UsuarioController.class, UsuarioDTO.class);
     }
 
+
     /** Converte classe UsuarioModel para classe UsuarioDTO */
     public UsuarioDTO convertToUsuarioDTO(UsuarioModel usuarioModel) {
         return toModel(usuarioModel);
     }
 
+
     @Override
     public UsuarioDTO toModel(UsuarioModel entity) {
         UsuarioDTO usuarioDTO = modelMapper.map(entity, UsuarioDTO.class);
 
-/*      Dessa forma usa methodOn() para referenciar diretamente os métodos com a URI mapeada da classe UsuarioController com o ID
-        especificado. Ajuda a evitar problemas caso a URL do método mude futuramente */
+        // Representa o URI indicando que este link aponta para o próprio usuario
+        usuarioDTO.add(usuarioLinks.addSelfLink(usuarioDTO));
 
-        usuarioDTO.add(WebMvcLinkBuilder            // adiciona o link HATEOAS ao objeto.
-            .linkTo(WebMvcLinkBuilder               // cria uma base para o link HATEOAS, apontando para o controlador UsuarioController
-                .methodOn(UsuarioController.class)  // é usado para referenciar um controlador e um método específico de forma segura.
-                .buscaPorId(usuarioDTO.getId()))    //  método do UsuarioController para detectar o mapeamento desse método e cria automaticamente a URL associada.
-            .withRel(IanaLinkRelations.SELF));     // Representa o URI indicando que este link aponta para o próprio usuario
+        // Representa o URI para a coleção de recursos do mesmo tipo do recurso atual do usuário
+        usuarioDTO.add(usuarioLinks.addCollectionLink());
 
-
-        usuarioDTO.add(WebMvcLinkBuilder            //  adiciona o link HATEOAS ao objeto.
-            .linkTo(WebMvcLinkBuilder.              // cria uma base para o link HATEOAS, apontando para o controlador UsuarioController
-                methodOn(UsuarioController.class)   // é usado para referenciar um controlador e um método específico de forma segura.
-                .lista())                           //  método do UsuarioController para detectar o mapeamento desse método e cria automaticamente a URL associada.
-            .withRel(IanaLinkRelations.COLLECTION)); // Representa o URI para a coleção de recursos do mesmo tipo do recurso atual do usuário
-
-
-        usuarioDTO.add(WebMvcLinkBuilder //  adiciona o link HATEOAS ao objeto.
-            .linkTo(WebMvcLinkBuilder               // cria uma base para o link HATEOAS, apontando para o controlador UsuarioGrupoController
-                .methodOn(UsuarioGrupoController.class)   // é usado para referenciar um controlador e um método específico de forma segura.
-                .lista(usuarioDTO.getId())) //  método do UsuarioGrupoController para detectar o mapeamento desse método e cria automaticamente a URL associada.
-            .withRel("grupos-usuarios"));    // Representa o URI indicando que este link aponta para o próprio recurso do grupo desse usuario
+        // Representa o URI indicando que este link aponta para o próprio recurso do grupo desse usuario
+        usuarioDTO.add(usuarioLinks.addSelfGruposUsuarioLink(usuarioDTO, "grupos-usuario"));
 
         return usuarioDTO;
     }
@@ -64,8 +53,6 @@ public class UsuarioDTOAssembler extends RepresentationModelAssemblerSupport<Usu
 
 //      Dessa forma adiciona a própria URI mapeada na classe UsuarioController para essa coleção
         return super.toCollectionModel(entities)
-            .add(WebMvcLinkBuilder
-                .linkTo(UsuarioController.class)
-                .withSelfRel());
+            .add(usuarioLinks.addSelfCollectionLink());
     }
 }
---
+++ src/main/java/com/algaworks/algafood/api/assemblers/links/UsuarioLinks.java
@@ -0,0 +1,56 @@
+package com.algaworks.algafood.api.assemblers.links;
+
+import com.algaworks.algafood.api.DTOs.UsuarioDTO;
+import com.algaworks.algafood.api.controllers.UsuarioController;
+import com.algaworks.algafood.api.controllers.UsuarioGrupoController;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioLinks {
+
+    /* Dessa forma usa methodOn() para referenciar diretamente os métodos com a URI mapeada da classe UsuarioController com o ID
+        especificado. Ajuda a evitar problemas caso a URL do método mude futuramente */
+
+    /** Cria link para o próprio objeto */
+    public Link addSelfLink(UsuarioDTO usuarioDTO){
+
+        return WebMvcLinkBuilder  // // adiciona o link HATEOAS ao objeto.
+            .linkTo(WebMvcLinkBuilder  // cria uma base para o link HATEOAS, apontando para o controlador UsuarioController
+                .methodOn(UsuarioController.class)  // é usado para referenciar um controlador e um método específico de forma segura.
+                .buscaPorId(usuarioDTO.getId())) // método do UsuarioController para detectar o mapeamento desse método e cria automaticamente a URL associada.
+            .withRel(IanaLinkRelations.SELF); // Representa o URI indicando que este link aponta para o próprio usuario
+    }
+
+    /** Cria link para a coleção desse objeto*/
+    public Link addCollectionLink(){
+
+        return WebMvcLinkBuilder       //  adiciona o link HATEOAS ao objeto.
+            .linkTo(WebMvcLinkBuilder.    // cria uma base para o link HATEOAS, apontando para o controlador UsuarioController
+                methodOn(UsuarioController.class)   // é usado para referenciar um controlador e um método específico de forma segura.
+                .lista())                  //  método do UsuarioController para detectar o mapeamento desse método e cria automaticamente a URL associada.
+            .withRel(IanaLinkRelations.COLLECTION); // Representa o URI para a coleção de recursos do mesmo tipo do recurso atual do usuario
+    }
+
+
+
+    /** Cria link para a coleção de grupos desse usuario */
+    public Link addSelfGruposUsuarioLink(UsuarioDTO usuarioDTO, String rel){
+
+        return WebMvcLinkBuilder //  adiciona o link HATEOAS ao objeto.
+            .linkTo(WebMvcLinkBuilder               // cria uma base para o link HATEOAS, apontando para o controlador UsuarioGrupoController
+                .methodOn(UsuarioGrupoController.class)   // é usado para referenciar um controlador e um método específico de forma segura.
+                .lista(usuarioDTO.getId())) //  método do UsuarioGrupoController para detectar o mapeamento desse método e cria automaticamente a URL associada.
+            .withRel(rel); // Representa o URI indicando que este link aponta para o próprio recurso do grupo desse usuario
+    }
+
+
+    /** Cria link da URI mapeada na classe UsuarioController para esse objeto */
+    public Link addSelfCollectionLink(){
+        return WebMvcLinkBuilder
+            .linkTo(UsuarioController.class)
+            .withSelfRel();
+    }
+}
