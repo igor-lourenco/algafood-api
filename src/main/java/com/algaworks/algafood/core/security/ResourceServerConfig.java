cb4b28e968c74567a19b37708eba3049913ba046
igor-lourenco <igor.lourencosantos@gmail.com>
2025-01-05 11:41:10 -0300

    Configurando o gerenciamento de sessao para STATELESS

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -34,15 +35,17 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 //                    .antMatchers("/v1/cidades/**").permitAll() // Permite o acesso sem autenticação para todas as URLs que começam com /v1/cidades/
                     .anyRequest().authenticated() // Requer autenticação para qualquer outra URL que não se encaixe nas regras anteriores.
 
+
+            .and()
+                .sessionManagement() // Permite configurar o gerenciamento de Sessão.
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Cria politica de sessão para ser 'STATELESS',
+            // significa que a aplicação não mantém estado entre as requisições.
+//          // Essa configuração é ideal para APIs RESTful, onde cada requisição é tratada de forma independente.
+//
+
             .and()
                 .oauth2ResourceServer()
                     .opaqueToken()
-
-//                .sessionManagement() // Permite configurar o gerenciamento de Sessão.
-//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Cria politica de sessão para ser 'STATELESS',
-//            // significa que a aplicação não mantém estado entre as requisições.
-//            // Essa configuração é ideal para APIs RESTful, onde cada requisição é tratada de forma independente.
-//
 //            .and()
 //                .csrf() // Proteção contra Cross-Site Request Forgery (CSRF), por padrão é habilitada (enabled)
 //                    .disable() //  Desabilita a proteção, pois essa proteção geralmente não é necessária em APIs REST que usam autenticação stateless.
