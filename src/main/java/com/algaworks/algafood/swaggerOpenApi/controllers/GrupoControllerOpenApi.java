27501519af3e5541fbe606c7c2fba9d0d3ddbcfe
igor-lourenco <igor.lourencosantos@gmail.com>
2025-03-16 10:07:49 -0300

    Descrevendo modelo de representação das respostas do GrupoController do endpoint - DELETE deleta grupo

--- src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/GrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/GrupoControllerOpenApi.java
@@ -19,7 +19,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**Essa interface é usada para gerar a documentação da API e definir os contratos dos endpoints relacionados a Grupo.*/
-//@Api(tags = "Grupos")
 @Tag(name = "Grupos")
 public interface GrupoControllerOpenApi {
 
@@ -45,10 +44,11 @@ public interface GrupoControllerOpenApi {
 
 
 
+    @ResponseStatus(value = HttpStatus.CREATED) // para visualização na documentação apenas o status code 201 de sucesso
     @Operation(summary = "Cadastra um novo grupo", responses = {
         @ApiResponse(responseCode = "200", description = "Grupo cadastrado com sucesso", content = @Content(schema = @Schema(implementation = GrupoHateoasOpenApi.class))),})
-    @ResponseStatus(value = HttpStatus.CREATED) // para visualização na documentação apenas o status code 201 de sucesso
     ResponseEntity<GrupoDTO> salva(@RequestBody(description = "Representação de um novo grupo", required = true) GrupoInput grupoInput);
+
 
 
     @Operation(summary = "Atualiza grupo pelo ID", responses = {
@@ -59,12 +59,10 @@ public interface GrupoControllerOpenApi {
         @RequestBody(description = "Representação do grupo com os novos dados", required = true) GrupoInput grupoInput);
 
 
-//
-//    @ApiResponses({
-//        @ApiResponse(code = 204, message = "Grupo deletado"),
-//        @ApiResponse(code = 400, message = "Requisição inválida (erro do cliente)", response = StandardErrorBadRequest.class),
-//        @ApiResponse(code = 404, message = "Grupo não encontrado", response = StandardErrorNotFound.class)})
-//    @ApiOperation(value = "Exclui grupo pelo ID")
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
-//    void deleta(@ApiParam(name = "id", value = "ID do grupo", example = "1", required = true) Long id);
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
+    @Operation(summary = "Exclui grupo pelo ID", responses = {
+        @ApiResponse(responseCode = "204", description = "Grupo deletado com sucesso", content = @Content(schema = @Schema)),})
+    void deleta(@Parameter(name = "grupoId", description = "ID do grupo", example = "1", required = true, schema = @Schema(type = "integer")) Long id);
+
 }
