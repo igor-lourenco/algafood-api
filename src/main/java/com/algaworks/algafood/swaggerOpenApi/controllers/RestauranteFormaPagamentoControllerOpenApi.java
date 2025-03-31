7d91544810a939b035e869392594aaf639ac455d
igor-lourenco <igor.lourencosantos@gmail.com>
2025-03-31 10:05:49 -0300

    Descrevendo modelo de representação das respostas do RestauranteFormapagamentoController do endpoint - GET busca lista de formas de pagamento do restaurante

--- src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/RestauranteFormaPagamentoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/RestauranteFormaPagamentoControllerOpenApi.java
@@ -1,21 +1,32 @@
 package com.algaworks.algafood.swaggerOpenApi.controllers;
 
-/**
- * Essa interface é usada para gerar a documentação da API e definir os contratos dos endpoints relacionados a Restaurante com as Formas de pagamento.
- */
-//@Api(tags = "Restaurantes")
+import com.algaworks.algafood.api.DTOs.FormaPagamentoDTO;
+import com.algaworks.algafood.swaggerOpenApi.exceptions.StandardErrorBadRequest;
+import com.algaworks.algafood.swaggerOpenApi.exceptions.StandardErrorNotFound;
+import com.algaworks.algafood.swaggerOpenApi.models.FormasPagamentoCollectionModelOpenApi;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+
+/** Essa interface é usada para gerar a documentação da API e definir os contratos dos endpoints relacionados a Restaurante com as Formas de pagamento. */
+@Tag(name = "Restaurantes")
 public interface RestauranteFormaPagamentoControllerOpenApi {
 
 
-//    @ApiOperation(value = "Busca lista de formas de pagamento do restaurante")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "Lista de formas de pagamento do restaurante encontrado", response = FormasPagamentoCollectionModelOpenApi.class),
-//        @ApiResponse(code = 400, message = "Requisição inválida (erro do cliente)", response = StandardErrorBadRequest.class),
-//        @ApiResponse(code = 404, message = "Restaurante não encontrado", response = StandardErrorNotFound.class),})
-//    ResponseEntity<CollectionModel<FormaPagamentoDTO>> buscaFormaPagamentoPorRestauranteId(
-//        @ApiParam(name = "restauranteId", value = "ID do restaurante", example = "1", required = true) Long restauranteId);
-//
-//
+    @Operation(summary = "Busca lista de formas de pagamento do restaurante", responses = {
+        @ApiResponse(responseCode= "200", description = "Lista de formas de pagamento do restaurante encontrado", content = @Content(schema = @Schema(implementation = FormasPagamentoCollectionModelOpenApi.class))),
+        @ApiResponse(responseCode= "400", description = "Requisição inválida (erro do cliente)", content = @Content(schema = @Schema(implementation = StandardErrorBadRequest.class))),
+        @ApiResponse(responseCode= "404", description = "Restaurante não encontrado", content = @Content(schema = @Schema(implementation = StandardErrorNotFound.class)))
+    })
+    ResponseEntity<CollectionModel<FormaPagamentoDTO>> buscaFormaPagamentoPorRestauranteId(
+        @Parameter(name = "restauranteId", description = "ID do restaurante", example = "1", required = true) Long restauranteId);
+
+
 //    @ApiOperation(value = "Associa forma de pagamento com restaurante")
 //    @ApiResponses({
 //        @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
