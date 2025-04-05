317f6f405179c071e28e684abba0e2b309385ef3
igor-lourenco <igor.lourencosantos@gmail.com>
2025-04-05 09:48:50 -0300

    Descrevendo modelo de representação das respostas do RestauranteProdutoController do endpoint - DELETE exclui a foto do produto do restaurante

--- src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/RestauranteProdutoFotoControllerOpenApi.java
@@ -14,8 +14,10 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -64,19 +66,21 @@ public interface RestauranteProdutoFotoControllerOpenApi {
         @Parameter(name = "produtoId", description = "ID do produto", example = "1", required = true) Long produtoId,
         @RequestBody FotoProdutoInput fotoProdutoInput,
         @Parameter(name = "arquivo", description = "arquivo da foto do produto (máximo 100KB, apenas JPG e PNG)", hidden = true) MultipartFile arquivo);
-//
-//
-//    @ApiOperation(value = "Exclui a foto do produto de um restaurante")
-//    @ApiResponses({
-//        @ApiResponse(code = 204, message = "Foto do produto de um restaurante deletado"),
-//        @ApiResponse(code = 400, message = "Requisição inválida (erro do cliente)", response = StandardErrorBadRequest.class),
-//        @ApiResponse(code = 404, message = "Restaurante, produto ou foto não encontrado", response = StandardErrorNotFound.class),})
-//    @ResponseStatus(HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
-//    void deletaFoto(
-//        @ApiParam(name = "restauranteId", value = "ID do restaurante", example = "1", required = true) Long restauranteId,
-//        @ApiParam(name = "produtoId", value = "ID do produto", example = "1", required = true) Long produtoId);
-//
-//
+
+
+
+    @Operation(summary = "Exclui a foto do produto de um restaurante",  responses = {
+        @ApiResponse(responseCode = "204", description = "Foto do produto de um restaurante deletado com sucesso", content = @Content(schema = @Schema)),
+        @ApiResponse(responseCode = "400", description = "Requisição inválida (erro do cliente)", content = @Content(schema = @Schema(implementation = StandardErrorBadRequest.class))),
+        @ApiResponse(responseCode = "404", description = "Restaurante ou produto não encontrado", content = @Content(schema = @Schema(implementation = StandardErrorNotFound.class)))
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
+    void deletaFoto(
+        @Parameter(name = "restauranteId", description = "ID do restaurante", example = "1", required = true) Long restauranteId,
+        @Parameter(name = "produtoId", description = "ID do produto", example = "1", required = true) Long produtoId);
+
+
+
     @Operation(summary = "API de teste para salvar foto do produto", hidden = true)
     void atualizarFotoTeste(
         @Parameter(name = "restauranteId", description = "ID do restaurante", example = "1", required = true) Long restauranteId,
