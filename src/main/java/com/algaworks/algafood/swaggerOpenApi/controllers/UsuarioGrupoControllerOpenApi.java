ec6df7648d6e1a39cf13ce6293a10d1c0263cf68
igor-lourenco <igor.lourencosantos@gmail.com>
2025-04-07 10:21:04 -0300

    Descrevendo modelo de represntacao do UsuarioGrupoController do endpoint - PUT associa grupo com usuario

--- src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/UsuarioGrupoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/swaggerOpenApi/controllers/UsuarioGrupoControllerOpenApi.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.swaggerOpenApi.controllers;
 
 import com.algaworks.algafood.api.DTOs.UsuarioGrupoDTO;
+import com.algaworks.algafood.swaggerOpenApi.exceptions.StandardErrorBadRequest;
 import com.algaworks.algafood.swaggerOpenApi.exceptions.StandardErrorNotFound;
 import com.algaworks.algafood.swaggerOpenApi.models.UsuarioGruposCollectionModelOpenApi;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,7 +11,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /** Essa interface é usada para gerar a documentação da API e definir os contratos dos endpoints relacionados a Usuários com grupo.*/
 @Tag(name = "Usuarios")
@@ -23,17 +26,18 @@ public interface UsuarioGrupoControllerOpenApi {
     })
     ResponseEntity<CollectionModel<UsuarioGrupoDTO>> lista(
         @Parameter(name = "usuarioId", description = "ID do usuário", example = "1", required = true) Long usuarioId);
-//
-//
-//    @ApiOperation(value = "Associação do grupo com usuário")
-//    @ApiResponses({
-//        @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
-//        @ApiResponse(code = 400, message = "Requisição inválida (erro do cliente)", response = StandardErrorBadRequest.class),
-//        @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado", response = StandardErrorNotFound.class),})
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
-//    void associa(
-//        @ApiParam(name = "usuarioId", value = "ID do usuário", example = "1", required = true) Long usuarioId,
-//        @ApiParam(name = "grupoId", value = "ID do grupo", example = "1", required = true) Long grupoId);
+
+
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT) // para visualização na documentação apenas o status code 204 de sucesso
+    @Operation(summary = "Associação do grupo com usuário", responses = {
+        @ApiResponse(responseCode = "204", description = "Associação realizada com sucesso", content = @Content(schema = @Schema)),
+        @ApiResponse(responseCode = "400", description = "Requisição inválida (erro do cliente)", content = @Content(schema = @Schema(implementation = StandardErrorBadRequest.class))),
+        @ApiResponse(responseCode = "404", description = "Usuário ou grupo não encontrado", content = @Content(schema = @Schema(implementation = StandardErrorNotFound.class)))
+    })
+    void associa(
+        @Parameter(name = "usuarioId", description = "ID do usuário", example = "1", required = true) Long usuarioId,
+        @Parameter(name = "grupoId", description = "ID do grupo", example = "1", required = true) Long grupoId);
 //
 //
 //    @ApiOperation(value = "Desassocia do grupo com usuário")
