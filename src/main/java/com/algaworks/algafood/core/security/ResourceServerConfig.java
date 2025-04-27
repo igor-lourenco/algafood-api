f6744724eba4c3f1d4b0e8e1681d745f143f85eb
igor-lourenco <igor.lourencosantos@gmail.com>
2025-04-27 11:34:09 -0300

    Lendo as claims customizadas do token JWT no Resource Server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,13 +1,23 @@
 package com.algaworks.algafood.core.security;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
 @Configuration
 @EnableWebSecurity // Habilita a configuração de segurança da web no Spring Security.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // Habilita a segurança de métodos em nível global, permitindo a utilização de anotações como @PreAuthorize e @PostAuthorize em seus métodos.
@@ -24,11 +34,42 @@ public class ResourceServerConfig {
             .csrf().disable() // Desativa proteção contra CSRF (Cross-Site Request Forgery) porque o ataque de CSRF geralmente depende de um navegador do usuário e de cookies de autenticação
             .cors() // Habilita suporte a CORS (Cross-Origin Resource Sharing).
             .and()
-            .oauth2ResourceServer().jwt(); // Configura a aplicação para usar tokens opacos (que precisam ser validados em um endpoint externo).
+            .oauth2ResourceServer()
+                .jwt() // Configura a aplicação para usar tokens jwt
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter()); // nossa imlementacao para ler o token jwt e pegar as informações
 
         return httpSecurity.formLogin(Customizer.withDefaults()).build();
     }
 
 
+    // Esse método é responsável por ler as informações customizadas do token JWT
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        log.info(">>> CARREGANDO BEAN PARA LER AS CLAIMS CUSTOMIZADAS DO TOKEN JWT");
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
 
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+            Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt); // lista de scope
+            log.info(">> Lista de scopes: {}", grantedAuthorities);
+
+            List<String> authorities = jwt.getClaimAsStringList("authorities"); // lista de authorities
+            log.info(">> Lista de authorities: {}", authorities);
+
+            if (authorities == null) {
+                return grantedAuthorities;
+            }
+
+
+            // junta a lista de authorities com a lista de scopes em uma só
+            grantedAuthorities.addAll(authorities
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList()));
+
+            return grantedAuthorities;
+        });
+
+        return converter;
+    }
 }
